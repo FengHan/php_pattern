f9770566e41fc6926c91935add1424765619615c
Michael <keke231@qq.com>
2015-08-13 19:05:29 +0800

    数据对象映射模式1

---
+++ framework/Vendor/User.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Vendor;
+
+
+class User
+{
+    public $id;
+    public $username;
+    public $email;
+    public $regtime;
+
+    protected $db;
+
+    public function __construct($id)
+    {
+        $this->db = new \Vendor\Database\MySQLi();
+        $this->db->connect('127.0.0.1', 'homestead', 'secret', 'homestead');
+        $res = $this->db->query('SELECT * FROM user LIMIT 1');
+        $data = $res->fetch_assoc();
+        $this->id = $data['id'];
+        $this->username = $data['username'];
+        $this->email = $data['email'];
+        $this->regtime = $data['regtime'];
+    }
+
+    public function __destruct()
+    {
+        $sql ='UPDATE user
+               SET username="%s",
+               email="%s",
+               regtime="%s"
+               WHERE id= %d';
+        $sql = sprintf($sql, $this->username, $this->email, $this->regtime, $this->id);
+        $this->db->query($sql);
+    }
+
+}
--- framework/index.php
+++ framework/index.php
@@ -3,9 +3,10 @@ define('BASEDIR', __DIR__);
 include BASEDIR . '/' . '/Vendor/Loader.php';
 spl_autoload_register('\\Vendor\\Loader::autoload');
 
-$strategy = isset($_GET['femail']) ? new \Vendor\FemaleUserStrategy() : new \Vendor\MaleUserStrategy();
-$page = new \Vendor\Page();
-$page->setStrategy($strategy);
 
-$page->index();
 
+$user = new \Vendor\User(1);
+
+$user->username = 'jack';
+$user->email = 'jack@example.com';
+$user->regtime = date('Y-m-d H:i:s');
